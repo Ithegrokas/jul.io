513e3bf23f2bb9cec57a656b28d46d2b002f00aa
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-05-28 11:54:49 -0400

    Demo for collision ignoring and similar

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private Type type;
+    public GameObject newObstacle;
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         Type julType = other.gameObject.GetComponent<Jul>().GetJulType();
         switch (julType)
         {
-            
-
+            case Type.Bri:
+                switch (type)
+                {
+                    case Type.Chan:
+                        Physics2D.IgnoreCollision(other.gameObject.GetComponent<Collider2D>(), this.GetComponent<Collider2D>());
+                        Debug.Log("Ignoring collision");
+                        break;
+                    case Type.Oli:
+                        Instantiate(this.newObstacle, this.transform.position, Quaternion.identity);
+                        Destroy(other.gameObject);
+                        Destroy(this.gameObject);
+                        break;
+                }
+                break;
         }
     }
 }
