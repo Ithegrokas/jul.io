fd20acbe954a8cee9b01b8bfe0a687ea839afd12
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-05-27 21:58:16 -0400

    Movement and collision stub

--- Assets/Scripts/Jul.cs
+++ Assets/Scripts/Jul.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Jul : MonoBehaviour
 {
     public bool notDragged = true;
+    public float speed = 5f;
+    public float speedMultiplier = 1f;
+    private Vector2 movement;
+    private Rigidbody2D playerRB;
 
-    void Update () 
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
     {
         if (!notDragged)
         {
-            
+            movement.y = 0;
+            movement.x = 1;
+            playerRB.velocity = movement * speed * speedMultiplier;
         }
     }
-
 }
